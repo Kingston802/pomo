ae8b8c24f1e7c41217a898cee694a3a809124bd2
Kingston802 <alexkingstonbailey@gmail.com>
2021-02-17 13:17:02 +1300

    fixed Task Input bar

--- src/components/Items.js
+++ src/components/Items.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import './Items.css'
 
 
@@ -15,20 +16,24 @@ function Items(props) {
     props.setItems(newArray);
   }
 
-  function handleButtonClick(c) {
+  function handleButtonClick() {
     // add value of input to list
     const text = document.querySelector('.task').value;
     const el = <li key={ text } onClick={handleItemClick}>{ text }</li>;
     props.setItems([...props.items, el]);
+
+    // clear input 
+    document.querySelector('.task').value = '';
   }
 
   const listItems = [...props.items];
 
   return (
     <ul>
-      <h2>Tasks</h2>
-      <input className="task"></input>
-      <button onClick={handleButtonClick}>Add</button>
+      <div className="TaskInput">
+        <input className="task" onKeyDown={(event) => { if (event.key === 'Enter') {handleButtonClick()} } }></input>
+        <button onClick={handleButtonClick}>➕</button>
+      </div>
       { listItems.length > 0 ? listItems : (<p>Add some items!</p>) }
     </ul>
   )
