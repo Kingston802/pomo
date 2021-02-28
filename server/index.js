a1366b757b8c5cf25a0cab982edc38e106d10efa
Kingston802 <alexkingstonbailey@gmail.com>
2021-02-28 16:40:45 +1300

    created basic create/delete functionality

--- server/index.js
+++ server/index.js
@@ -1,15 +1,38 @@
 const express = require('express');
 const app = express();
-const { readFile } = require('fs');
+const { readFile, writeFile } = require('fs');
+const { exec } = require("child_process");
+const { exit } = require('process');
+
+try {
+    let file = require('./db.json');
+} catch {
+    exec(`echo '{"tasks": []}' > ./db.json`, (err, stdo, stde) => {
+        if (err) {
+            console.error(err);
+            exit();
+        }
+    })    
+}
+let file = require('./db.json');
 
 // create 
 app.post('/api/items', (request, response) => {
-    if (err) {
-        console.error(err);
-        response.sendStatus(500); // equivalent to res.status(500).send('Internal Server Error')
-    }
+    // make changes 
+    file.tasks.push(request.query.task);
 
-    console.log('create request!');
+    // write changes to file 
+    writeFile('./db.json', JSON.stringify(file), (err) => {
+        if (err) {
+            console.error(err);
+            response.sendStatus(500); // equivalent to res.status(500).send('Internal Server Error')
+        }
+        console.log(JSON.stringify(file));
+        console.log('writing to db.json');
+    });
+
+    // respond with file 
+    response.send(file);
 });
 
 // read 
@@ -36,13 +59,30 @@ app.put('/api/items', (request, response) => {
 });
 
 // delete 
-app.delete('/api/items', (request, reponse) => {
-    if (err) {
-        console.error(err);
-        response.sendStatus(500) // equivalent to res.status(500).send('Internal Server Error')
-    }
+app.delete('/api/items', (request, response) => {
+    // make changes 
+    let deleted = false;
+    file.tasks = file.tasks.filter(task => {
+        if (task === request.query.task && !deleted) {
+            deleted = true;
+            return false;
+        } else {
+            return true;
+        }
+    });
 
-    console.log('delete request!');
+    // write changes to file 
+    writeFile('./db.json', JSON.stringify(file), (err) => {
+        if (err) {
+            console.error(err);
+            response.sendStatus(500); // equivalent to res.status(500).send('Internal Server Error')
+        }
+        console.log(JSON.stringify(file));
+        console.log('writing to db.json');
+    });
+
+    // respond with file 
+    response.send(file);
 });
 
 
